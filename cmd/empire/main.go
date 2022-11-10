2ae16afd7df2c0e5dd60947d9e0ceb9a63a5829e
Justin Godden <justingodden@gmail.com>
2022-11-10 17:03:56 +0000

    feat(repl): add repl structure for lexing only

--- cmd/empire/main.go
+++ cmd/empire/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/justingodden/empire/pkg/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Empire progamming Language!\n", user.Username)
+	fmt.Print("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
