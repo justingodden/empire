bfc6080d1514ec26f0977f63cee27c7efac1d059
Justin Godden <justingodden@gmail.com>
2022-11-16 21:54:18 +0000

    feat(while): add while loops

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -221,6 +221,27 @@ func (ie *IfExpression) String() string {
 	return out.String()
 }
 
+type WhileExpression struct {
+	Token       token.Token // the 'while' token
+	Condition   Expression
+	Consequence *BlockStatement
+}
+
+func (we *WhileExpression) expressionNode()      {}
+func (we *WhileExpression) TokenLiteral() string { return we.Token.Literal }
+func (we *WhileExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("while ")
+	out.WriteString("(")
+	out.WriteString(we.Condition.String())
+	out.WriteString(") { ")
+	out.WriteString(we.Consequence.String())
+	out.WriteString(" }")
+
+	return out.String()
+}
+
 type FunctionLiteral struct {
 	Token      token.Token // the 'fn' token
 	Parameters []*Identifier
